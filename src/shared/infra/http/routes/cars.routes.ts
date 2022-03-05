50cffe95e743e60018658962b1f7b255d794b82c
AlissonSantos17 <alisson.wiin@gmail.com>
2022-03-05 13:11:09 -0300

    create findAvailable

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCars";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCars = new ListAvailableCarsController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
+
+carsRoutes.get("/available", listAvailableCars.handle);
 
 export { carsRoutes };
