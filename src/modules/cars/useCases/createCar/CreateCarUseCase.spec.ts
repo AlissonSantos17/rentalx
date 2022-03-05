46ed2df2824f56488b531b3d17edad8e9860abd7
AlissonSantos17 <alisson.wiin@gmail.com>
2022-03-05 01:17:02 -0300

    create car with useCase, controller and tests

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -12,7 +12,7 @@ describe("Create Car", () => {
     createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
   });
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car",
       description: "Description Car",
       daily_rate: 100,
@@ -21,6 +21,8 @@ describe("Create Car", () => {
       brand: "Brand",
       category_id: "category",
     });
+
+    expect(car).toHaveProperty("id");
   });
 
   it("should not be able to crate a car with exists license plate", () => {
